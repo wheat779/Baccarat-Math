4d9ed312f6ac5652741777356c4d665f51da251d
Cameron K <cameronk@camerons-macbook-pro.local>
2015-10-04 17:12:22 -0700

    Initial Commit

---
+++ Baccarat Math/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Baccarat Math
+//
+//  Created by Cameron K on 10/4/15.
+//  Copyright © 2015 Cameron K. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
